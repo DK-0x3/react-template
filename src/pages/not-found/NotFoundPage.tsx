3b81bc35f6c950584294bc5fb0b75beac7de0a04
Dmitry Kezarev <dkezarev516@gmail.com>
2025-09-29 16:51:36 +0500

    Дорабатывает NotFoundPage

--- src/pages/not-found/NotFoundPage.tsx
+++ src/pages/not-found/NotFoundPage.tsx
@@ -1,11 +1,13 @@
 import ROUTES from '@shared/configs/routes';
 import { useTranslation } from 'react-i18next';
 
+import styles from './NotFoundPage.module.scss';
+
 const NotFoundPage = () => {
 	const { t } = useTranslation();
     
 	return (
-		<div>
+		<div className={styles.wrapper}>
 			<h1>{t('Ошибка, страница не найдена')}</h1>
 			<a href={ROUTES.HOME}>{t('На главную')}</a>
 		</div>
